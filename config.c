064d3fe92d3d64ace1e6ecde921fff7b714ce47d
John Osinski <johnosinski80@gmail.com>
2023-05-12 15:44:20 -0400

    Initial Load

---
+++ config.c
@@ -0,0 +1,11 @@
+#include "config.h"
+
+#include "block.h"
+#include "util.h"
+
+Block blocks[] = {
+    {"sb-battery", 5,    1 },
+    {"sb-date",    1,    2 },
+};
+
+const unsigned short blockCount = LEN(blocks);
