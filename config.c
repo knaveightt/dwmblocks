c1586b7f42f7088049e0965e6a3264c2de452525
John Osinski <johnosinski80@gmail.com>
2023-05-19 11:45:53 -0400

    reordered status indicators

--- config.c
+++ config.c
@@ -4,9 +4,9 @@
 #include "util.h"
 
 Block blocks[] = {
+    {"spade-bar-ram",     60,    3 },
     {"spade-bar-batt",    60,    1 },
     {"spade-bar-date",    60,    2 },
-    {"spade-bar-ram",     60,    3 },
 };
 
 const unsigned short blockCount = LEN(blocks);
