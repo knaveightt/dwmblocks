60de6edf93d0b29596b1de6723f80ae8591b5162
John Osinski <johnosinski80@gmail.com>
2023-05-19 11:45:12 -0400

    added ram usage indicator to status

--- config.c
+++ config.c
@@ -4,8 +4,9 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"spade-bar-batt",    5,    1 },
-    {"spade-bar-date",    1,    2 },
+    {"spade-bar-batt",    60,    1 },
+    {"spade-bar-date",    60,    2 },
+    {"spade-bar-ram",     60,    3 },
 };
 
 const unsigned short blockCount = LEN(blocks);
